eaf3389f0cbb595cb581680c6d918c644530c304
joaolucas01 <jlmota0412@gmail.com>
2025-02-18 15:06:16 -0300

    .

--- aula011/ex008.js
+++ aula011/ex008.js
@@ -1,1 +1,6 @@
-console.log('o console está funcionando corretamente')
+var vel = 78.2
+console.log(`a velocidade do seu carro é de ${vel}km/h`)
+if (vel > 60){
+    console.log('você ultrapassou o limite de velocidade. MULTADO!')
+} 
+console.log('dirija sempre usando cinto de segurança')
