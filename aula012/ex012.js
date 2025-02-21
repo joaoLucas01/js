dc5b5d8643506864c6c1d532a8c75885dcaf38c1
joaolucas01 <jlmota0412@gmail.com>
2025-02-21 19:20:00 -0300

    .

---
+++ aula012/ex012.js
@@ -0,0 +1,11 @@
+//condições aninhadas
+
+var idade = 37
+if (idade < 16){
+    console.log('não vota')
+} else if(idade < 18 || idade > 65){
+        console.log('voto opcional')
+} else {
+        console.log('vota')    
+    }
+    
---
+++ aula012/ex013.js
@@ -0,0 +1,12 @@
+//condições aninhadas
+
+var agora = new Date
+var hora = agora.getHours()
+console.log(`agora são exatamente ${hora} horas`)
+if (hora >= 6 && hora <= 12){
+    console.log('bom dia!')
+} else if (hora <= 18) {
+    console.log('boa tarde')
+} else {
+    console.log('boa noite')
+}
